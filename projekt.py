97dc2fa6edbc369020e19595d256e727b4e2cbea
s189450 <s189450@student.pg.edu.pl>
2025-01-21 19:37:57 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -1,6 +1,13 @@
+from cProfile import label
 from math import ceil
-import numpy as py
-import matplotlib as mpl
+from typing import final
+import importlib.resources
+import numpy as np
+import matplotlib.pyplot as plt
+from pkg_resources import non_empty_lines
+from sympy.codegen.ast import continue_, break_
+from matplotlib.widgets import TextBox, Button
+from sympy.simplify.hyperexpand import try_lerchphi
 
 #Wszystkie zmienne dane które podawane będą z klawiatury:
 #Temperatura ciała i temperatura wody (początkowe)
@@ -19,83 +26,181 @@ rho = 1 #kg/m^3
 Cw = 4200 #J/kgK
 
 def termometr(temkoncowa):
-    if temkoncowa > 275.15 and temkoncowa < 375.15:
+    if temkoncowa > 273.15 and temkoncowa < 373.15:
         return 0
-    elif temkoncowa >= 375.15:
+    elif temkoncowa > 373.15:
         return 1
     else:
         return 2
 
+def CzyDobraObjetosc(To, m, Cc, Cw, Tc, Tw):
+    if termometr(To) == 1:
+        V = ((m * Cc) / (rho * Cw)) * ((Tc - Tw) / (373.15 - Tw) - 1)
+        return print("Woda zaczyna wrzec, masz za mala objetosc basenu! Minimalna wielkosc basenu to: ", ceil(V), "m^3")
+    elif termometr(To) == 2:
+        V = ((m * Cc) / (rho * Cw)) * ((Tc - Tw) / (273.15 - Tw) - 1)
+        return print("Woda zaczyna krzepnac, masz za mala objetosc basenu! Minimalna wielkosc basenu to: ", ceil(V), "m^3")
+    else:
+        return termometr(To)
+
+def CzyLiczbaDobraInator(Zmienna):
+    while True:
+        try:
+            Zmienna = float(Zmienna)
+        except:
+            print("Podaj liczbe:")
+            Zmienna = CzyLiczbaDobraInator(input())
+        else:
+            Zmienna = float(Zmienna)
+            break
+    while Zmienna <= 0:
+        print("Podaj poprawna wartosc:")
+        Zmienna = CzyLiczbaDobraInator(input())
+    return float(Zmienna)
+
+def CzyLiczbaDobraInator2(Zmienna):
+    while True:
+        try:
+            Zmienna = float(Zmienna)
+        except:
+            print("Podaj liczbe:")
+            break
+        else:
+            if Zmienna <= 0:
+                print("Podaj poprawna wartosc:")
+                break
+            if Zmienna >0:
+                return Zmienna
+def CzyDobraTemperatura(Zmienna):
+    Zmienna = CzyLiczbaDobraInator(Zmienna)
+    while Zmienna < 273.15 or Zmienna > 373.15:
+        print("Woda nie ma odpowiedniej temperatury. Popraw:")
+        Zmienna = CzyLiczbaDobraInator(input())
+    return Zmienna
+def CzyDobraTemperatura2(Zmienna):
+    Zmienna = CzyLiczbaDobraInator2(Zmienna)
+    while True:
+        if Zmienna < 273.15 or Zmienna > 373.15:
+            print("Woda nie ma odpowiedniej temperatury. Popraw:")
+            break
+        elif 273.15 <= Zmienna <= 373.15:
+            return Zmienna
+
+'''
 print("Podaj dane dla ciala:")
-while True:
-    try:
-        print("temperatura poczatkowa [K]:")
-        Tc = float(input())
-    except ValueError:
-        print("Podaj wielkość liczbową")
-        continue
-    else:
-        break
-while True:
-    try:
-        print("masa [kg]:")
-        m = float(input())
-    except ValueError:
-        print("Podaj wielkość liczbową")
-        continue
-    else:
-        break
-while True:
-    try:
-        print("cieplo wlasciwe [J/kgK]:")
-        Cc = float(input())
-    except ValueError:
-        print("Podaj wielkość liczbową")
-        continue
-    else:
-        break
-
+print("temperatura poczatkowa [K]:")
+Tc = CzyLiczbaDobraInator(input())
+print("masa [kg]:")
+m = CzyLiczbaDobraInator(input())
+print("cieplo wlasciwe [J/kgK]:")
+Cc = CzyLiczbaDobraInator(input())
 print("Podaj dane dla basenu:")
-while True:
+print("temperatura poczatkowa [K]:")
+Tw = CzyDobraTemperatura(input())
+print("objetosc [m^3]:")
+V = CzyLiczbaDobraInator(input())
+'''
+Tc = float(100)
+m = float(100)
+Cc = float(100)
+Tw = float(300)
+V= float(100)
+def Wyniki(Tc, m, Cc, Tw, V):
+    if Tw > Tc:
+        dQ = (Tw - Tc)*(rho*V*m*Cw*Cc)/(m*Cc + rho * V * Cw)
+        To = (-dQ)/(rho*V*Cw)+Tw
+        dS = m*Cc*np.log(abs(To/Tc)) + rho*V*Cw*np.log(abs(To/Tw))
+        if CzyDobraObjetosc(To, m, Cc, Cw, Tc, Tw) == 0:
+            print("dQ[J] = ", round(dQ, 2), ",", "To[K] = ", round(To, 2), ",", "dS[J/K] = ", round(dS, 2))
+            print(f"Ciepło właściwe:{Cc},Temperatura ciała: {Tc}, Masa: {m}, Temperatura wody: {Tw}, Objętość: {V}\n")
+            # Wykresy
+            TemCialoX = np.linspace(Tc, To, 100)
+            CieploCialoY = m * Cc * (TemCialoX - Tc)
+
+            TemWodaX = np.linspace(Tw, To, 100)
+            CieploWodaY = rho * V * Cw * (-TemWodaX + Tw)
+            return dQ, To, CieploCialoY, CieploWodaY, TemCialoX, TemWodaX, dS
+    if Tw < Tc:
+        dQ = (Tc - Tw)*(rho*V*m*Cw*Cc)/(m*Cc + rho * V * Cw)
+        To = (dQ)/(rho*V*Cw)+Tw
+        dS =  m*Cc*np.log(abs(To/Tc)) + rho*V*Cw*np.log(abs(To/Tw))
+        if CzyDobraObjetosc(To, m, Cc, Cw, Tc, Tw) == 0:
+            print("dQ[J] = ", round(dQ, 2), ",", "To[K] = ", round(To, 2), ",", "dS[J/K] = ", round(dS, 2))
+            print(f"Ciepło właściwe:{Cc},Temperatura ciała: {Tc}, Masa: {m}, Temperatura wody: {Tw}, Objętość: {V}\n")
+            # Wykresy
+            TemCialoX = np.linspace(Tc, To, 100)
+            CieploCialoY = m * Cc * (-TemCialoX + Tc)
+            TemWodaX = np.linspace(Tw, To, 100)
+            CieploWodaY = rho * V * Cw * (TemWodaX - Tw)
+            return dQ, To, CieploCialoY, CieploWodaY, TemCialoX, TemWodaX, dS
+dQ1, To1, CieploCialoY, CieploWodaY, TemCialoX, TemWodaX, dS1 = Wyniki(Tc, m, Cc, Tw, V)
+
+fig, ax = plt.subplots()
+line1, = ax.plot(TemCialoX, CieploCialoY, color='y', lw=1, ls='--', label='Qciala(T)')
+line2, = ax.plot(TemWodaX, CieploWodaY, color='b', lw=1, ls='--', label='Qwody(T)')
+ax.legend()
+ax.set_xlabel('T[K]')
+ax.set_ylabel('dQ[J]')
+ax.set_title('Wykres funkcji przeplywu ciepla do temperatury Q(T):')
+ax.grid(True)
+
+annotation = None
+def update(operation, argument):
+   global annotation, Tc, m ,Cc, Tw, V
+   while True:
+       try:
+            if operation == 'Cc':
+                Cc = CzyLiczbaDobraInator2(argument)
+                dQ2, To2, CieploCialoY2, CieploWodaY2, TemCialoX2, TemWodaX2, dS2 = Wyniki(Tc, m, Cc, Tw, V)
+            elif operation == 'Tc':
+                Tc = CzyLiczbaDobraInator2(argument)
+                dQ2, To2, CieploCialoY2, CieploWodaY2, TemCialoX2, TemWodaX2, dS2 = Wyniki(Tc, m, Cc, Tw, V)
+            elif operation == 'm':
+                m = CzyLiczbaDobraInator2(argument)
+                dQ2, To2, CieploCialoY2, CieploWodaY2, TemCialoX2, TemWodaX2, dS2 = Wyniki(Tc, m, Cc, Tw, V)
+            elif operation ==  'Tw':
+                Tw = CzyDobraTemperatura2(argument)
+                dQ2, To2, CieploCialoY2, CieploWodaY2, TemCialoX2, TemWodaX2, dS2 = Wyniki(Tc, m, Cc, Tw, V)
+            elif operation == 'V':
+                V = CzyLiczbaDobraInator2(argument)
+                dQ2, To2, CieploCialoY2, CieploWodaY2, TemCialoX2, TemWodaX2, dS2 = Wyniki(Tc, m, Cc, Tw, V)
+            else:
+                print("Invalid operation")
+            if annotation is not None:
+                annotation.remove()
+            line1.set_xdata(TemCialoX2)
+            line1.set_ydata(CieploCialoY2)
+            line2.set_xdata(TemWodaX2)
+            line2.set_ydata(CieploWodaY2)
+            ax.set_title(f'Wykres funkcji przeplywu ciepla do temperatury Q(T).')
+            if Tw < Tc:
+                ax.set_xlim([Tw - Tw / 10, Tc + Tc / 10])
+            elif Tw > Tc:
+                ax.set_xlim([Tc - Tc / 10, Tw + Tw / 10])
+            ax.set_ylim([-dQ2 / 10, dQ2 + dQ2 / 8])
+            annotation = ax.annotate(f'T[K]{To2:.0f}, dQ: {dQ2:.0f}', xy=(To2, dQ2), xytext=(To2 - To2/7, dQ2 + dQ2 / 15),
+                                     arrowprops=dict(facecolor='black', shrink=0.01),
+                                     )
+       except:
+            break
+       finally:
+           break
+   fig.canvas.draw_idle()
+operations = ["Cc", "Tc", "m", "Tw", "V"]
+text_boxes = {}
+def test(operation, text):
     try:
-        print("temperatura poczatkowa [K]:")
-        Tw = float(input())
+        number = float(text)
     except ValueError:
-        print("Podaj wielkość liczbową")
-        continue
-    else:
-        break
-while True:
-    try:
-        print("objetosc [m^3]:")
-        V = float(input())
-    except ValueError:
-        print("Podaj wielkość liczbową")
-        continue
-    else:
-        break
+        print("Proszę podać cyfrę")
+        return
+    update(operation, number)
 
-if Tw > Tc:
-    dQ = (Tw - Tc)*(rho*V*m*Cw*Cc)/(m*Cc + rho * V * Cw)
-    To = (-dQ)/(rho*V*Cw)+Tw
-    dS = dQ*(Tw-Tc)/(Tc*Tw)
-    print(round(dQ, 2), round(To,2), round(dS,2))
-
-if Tw < Tc:
-    dQ = (Tc - Tw)*(rho*V*m*Cw*Cc)/(m*Cc + rho * V * Cw)
-    To = (dQ)/(rho*V*Cw)+Tw
-    dS = dQ*(Tc-Tw)/(Tc*Tw)
-    print(round(dQ, 2), round(To, 2), round(dS, 2))
-
-if termometr(To) == 1:
-    V_2 = ((m*Cc)/(rho*Cw))*((Tc-Tw)/(375.15 - Tw) - 1)
-    if Tw < To:
-        print("Woda zaczyna wrzec, masz za mala objetosc basenu! Minimalna wielkosc basenu to: ", V)
-    elif Tw > To:
-        if V_2 > V:
-            print("Woda nie przestaje wrzeć, minimalna objętość:", V_2)
-        elif V_2 < V
-            print('Woda przestaje wrzeć....?')
-elif termometr(To) == 2:
-    V_2 = ((m*Cc)/(rho*Cw))*((Tc-Tw)/(275.15 - Tw) - 1)
-    print("Woda zaczyna krzepnac, masz za mala objetosc basenu! Minimalna wielkosc basenu to: ", V, "m^3")
+for i, operation in enumerate(operations):
+    axbox = plt.axes([0.03, 0.9 - i*0.12, 0.05, 0.075])
+    text_box = TextBox(axbox, f"{operation.capitalize()}: ")
+    text_box.on_submit(lambda text, op=operation: test(op, text))
+    text_boxes[operation] = text_box
+annotation = ax.annotate(f'T[K]{To1:.0f}, dQ: {dQ1:.0f}', xy=(To1, dQ1), xytext=(To1,dQ1+dQ1/15),
+                   arrowprops=dict(facecolor='black', shrink=0.05))
+plt.show()
